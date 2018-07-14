667aff3a0039d163f25e2ed78462dc01708326e2
Jan Xie <jan.h.xie@gmail.com>
2018-07-14 21:33:11 +0800

    different log level for tests

--- lib/ckb/logger.rb
+++ lib/ckb/logger.rb
@@ -2,8 +2,16 @@ require 'lumberjack'
 
 module CKB
 
-  def self.get_logger(progname=nil)
-    Lumberjack::Logger.new(STDOUT, progname: progname)
+  def self.default_log_level
+    @default_log_level ||= :info
+  end
+
+  def self.default_log_level=(level)
+    @default_log_level = level
+  end
+
+  def self.get_logger(progname=nil, level=default_log_level)
+    Lumberjack::Logger.new(STDOUT, progname: progname, level: level)
   end
 
   Logger = get_logger(self.name)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "ckb"
 
 require "minitest/autorun"
+
+CKB.default_log_level = :fatal
